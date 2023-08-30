fbe492bd1208c1810e6e285ee2f928bd3a717fd2
Luciano <lunick997@gmail.com>
2023-08-30 12:26:18 +0200

    commit

--- src/assets/Fetch.jsx
+++ src/assets/Fetch.jsx
@@ -4,22 +4,33 @@ import { useEffect, useState } from "react";
 const Fetch = (username) => {
     const [data, setData] = useState("")
     const [load,setLoad] = useState(true)
+    const [error, setError] = useState();
     async function fetchData(username) {
         try {
             const response = await fetch(`https://api.github.com/users/${username}`)
             const json = await response.json()
             setData(json)
             setLoad(false)
+
       } catch (error) {
             setError(error)
+            setLoad(false)
         }
     }
     useEffect(() => {
-        fetchData(username)
+        fetchData(username);
+        if (!username) {
+            setLoad(false);
+            return;
+          }
+      
+          setLoad(true);
+          setError(null);
+          setData(null);
     }, [username])
+    
     return (
-        data,
-        load
+        {data,load,error, fetchData}
     )
 }
 export default Fetch
--- src/assets/UserGit.jsx
+++ src/assets/UserGit.jsx
@@ -1,11 +1,16 @@
 import Fetch from "./Fetch"
+import { useEffect } from "react"
 function UserGit({ username }) {
-    const data = Fetch(username)
-    console.log(data)
-    const load = Fetch(username)
+    const { data, load, error, fetchData } = Fetch(username)
+    console.log(username)
+    useEffect(() => {
+        fetchData(username)
+    }, [username])
     return (
         <div>
-            {load === true? <p>loading...</p> : <h1>{data.name}</h1>}
+            {data && <h1>{data.name}</h1>}
+            {load && <h1>Loading...</h1>}
+            {error && <h1>error!</h1>}
         </div>
     )
 }
